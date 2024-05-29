23d185c934b55c0627cc1352d102c74301a26f4a
Miata <83115606+Oreo-bot0@users.noreply.github.com>
2024-05-29 19:13:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const balloonContainer = document.getElementById("balloon-container");
+
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color
